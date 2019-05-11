f5247960613c6c9c04b1738b89ce0c7ffa691e9e
Daniel Maslowski <dan@orangecms.org>
2019-05-11 20:08:59 +0200

    try setting thermal thresholds

--- src/mainboard/gigabyte/p34v2/acpi_tables.c
+++ src/mainboard/gigabyte/p34v2/acpi_tables.c
@@ -17,9 +17,38 @@
 
 #include <southbridge/intel/lynxpoint/nvs.h>
 
+#include "thermal.h"
+
+static global_nvs_t *gnvs_;
+
+static void acpi_update_thermal_table(global_nvs_t *gnvs)
+{
+	gnvs->f4of = FAN4_THRESHOLD_OFF;
+	gnvs->f4on = FAN4_THRESHOLD_ON;
+
+	gnvs->f3of = FAN3_THRESHOLD_OFF;
+	gnvs->f3on = FAN3_THRESHOLD_ON;
+
+	gnvs->f2of = FAN2_THRESHOLD_OFF;
+	gnvs->f2on = FAN2_THRESHOLD_ON;
+
+	gnvs->f1of = FAN1_THRESHOLD_OFF;
+	gnvs->f1on = FAN1_THRESHOLD_ON;
+
+	gnvs->f0of = FAN0_THRESHOLD_OFF;
+	gnvs->f0on = FAN0_THRESHOLD_ON;
+
+	gnvs->tcrt = CRITICAL_TEMPERATURE;
+	gnvs->tpsv = PASSIVE_TEMPERATURE;
+	gnvs->tmax = MAX_TEMPERATURE;
+	gnvs->flvl = 5;                     // Fan level
+}
+
 /* FIXME: check this function.  */
 void acpi_create_gnvs(global_nvs_t *gnvs)
 {
+	gnvs_ = gnvs;
+
 	/* Disable USB ports in S3 by default */
 	gnvs->s3u0 = 0;
 	gnvs->s3u1 = 0;
@@ -31,6 +60,8 @@ void acpi_create_gnvs(global_nvs_t *gnvs)
 	// the lid is open by default.
 	gnvs->lids = 1;
 
-	gnvs->tcrt = 100;
-	gnvs->tpsv = 90;
+	gnvs->tcrt = CRITICAL_TEMPERATURE;
+	gnvs->tpsv = PASSIVE_TEMPERATURE;
+
+	acpi_update_thermal_table(gnvs);
 }
